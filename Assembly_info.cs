c73c04fb5f5faa335641cb48742fa954267f455a
עילי גם זו לטובה <ilai.gamzu13@gmail.com>
2023-08-17 16:33:12 +0000

    Updated Assembly_info.cs

--- Assembly_info.cs
+++ Assembly_info.cs
@@ -4,10 +4,12 @@ using System.Runtime.InteropServices;
 
 namespace Assembly_info{
     internal class Assembly_info{
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+        public static void (){        
+            [assembly: AssemblyDescription("")]
+            [assembly: AssemblyCompany("")]
+            [assembly: AssemblyProduct("")]
+            [assembly: AssemblyVersion("1.0.0.0")]
+            [assembly: AssemblyFileVersion("1.0.0.0")]
+}
     }
 }
