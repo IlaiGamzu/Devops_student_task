e644975f35b08c04dc14993b6eecd7d3e6521c74
עילי גם זו לטובה <ilai.gamzu13@gmail.com>
2023-08-17 10:09:01 +0000

    Updated custom_1.ps1 try1

--- custom_1.ps1
+++ custom_1.ps1
@@ -1,83 +1,50 @@
-# Path to the AssemblyInfo.cs file
-#$assemblyInfoPath = "C:\C#_PROGRAM\Devops student task IAI\custom_student_task\copy_project_of_assemblyinfo.cs"
-#$assemblyInfoPath = "C:\C#_PROGRAM\Devops student task IAI\custom_student_task\Assembly_info\Program.cs"
-#$assemblyInfoPath= "https://dev.azure.com/ilaigamzu13/Devops_student_task/_git/DevOps_task3?path=/project_2/Assembly_info.cs&version=GBmaster"
-
-
 # Get the directory where the script is located
 $scriptDirectory = $PSScriptRoot
 
 # Define the relative path to the AssemblyInfo.cs file
-#$assemblyInfoPath = Join-Path $scriptDirectory "Assembly_info\Program.cs"
-#$assemblyInfoPath= $(Build.SourcesDirectory)
-# Read the content of AssemblyInfo.cs
-#$content = Get-Content -Path $assemblyInfoPath
-
-# Value to replace with from pipeline
-#param(
-#    [string]$ValueFromPipeline_1,
-#    [string]$ValueFromPipeline_2,
-#    [string]$ValueFromPipeline_3
-#)
-
-# Define regex patterns to match AssemblyDescription and AssemblyCompany attributes
-$pattern_des = '(\[assembly: AssemblyDescription\("")|(\[assembly: AssemblyDescription\(")([^"]*)("\)\])'
-$pattern_com = '(\[assembly: AssemblyCompany\("")|(\[assembly: AssemblyCompany\(")([^"]*)("\)\])'
-$pattern_pro = '(\[assembly: AssemblyProduct\("")|(\[assembly: AssemblyCompany\(")([^"]*)("\)\])'
-$pattern_ver = '(\[assembly: AssemblyVersion\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
-$pattern_ver_file = '(\[assembly: AssemblyFileVersion\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
-# Process each line from the file
-#$newContent = 
-#foreach ($line in $content) {
-$directories = Get-ChildItem -Directory
+$assemblyInfoPattern = "AssemblyInfo.*.cs"
+$directories = Get-ChildItem -Path $scriptDirectory -Recurse -Include $assemblyInfoPattern -Directory
 
 foreach ($directory in $directories) {
-    $assemblyInfoFiles = Get-ChildItem -Path $directory.FullName -Recurse -Include AssemblyInfo.*
+    $assemblyInfoFiles = Get-ChildItem -Path $directory.FullName -Recurse -Include $assemblyInfoPattern
     foreach ($file in $assemblyInfoFiles) {
-        # Perform operations on each assembly info file
-
-    if ($file -match $pattern_des) {
         $content = Get-Content -Path $file.FullName
-        $newContent = $content -replace $pattern_des, ('[assembly: AssemblyDescription("{0}")]' -f $ValueFromPipeline_1)
-        $newContent | Set-Content -Path $file.FullName
+
+        # Define regex patterns to match attributes
+        $pattern_des = '(\[assembly: AssemblyDescription\("")|(\[assembly: AssemblyDescription\(")([^"]*)("\)\])'
+        $pattern_com = '(\[assembly: AssemblyCompany\("")|(\[assembly: AssemblyCompany\(")([^"]*)("\)\])'
+        $pattern_pro = '(\[assembly: AssemblyProduct\("")|(\[assembly: AssemblyCompany\(")([^"]*)("\)\])'
+        $pattern_ver = '(\[assembly: AssemblyVersion\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
+        $pattern_ver_file = '(\[assembly: AssemblyFileVersion\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
+
+        $newContent = $content
+
+        if ($content -match $pattern_des) {
+            $newContent = $newContent -replace $pattern_des, ('[assembly: AssemblyDescription("{0}")]' -f $ValueFromPipeline_1)
         }
         
-    }
-    elseif ($file -match $pattern_com) {
-        $content = Get-Content -Path $file.FullName
-        $newContent = $content -replace $pattern_com, ('[assembly: AssemblyCompany("{0}")]' -f $ValueFromPipeline_2)
-        $newContent | Set-Content -Path $file.FullName
+        if ($content -match $pattern_com) {
+            $newContent = $newContent -replace $pattern_com, ('[assembly: AssemblyCompany("{0}")]' -f $ValueFromPipeline_2)
+        }
         
-    }
-    elseif ($file -match $pattern_pro) {
-         $content = Get-Content -Path $file.FullName
-         $newContent = $content -replace $pattern_pro, ('[assembly: AssemblyCompany("{0}")]' -f $ValueFromPipeline_3)
-         $newContent | Set-Content -Path $file.FullName
-    }
-    elseif($file -match $pattern_ver){
-        $content = Get-Content -Path $file.FullName
-        $majorMinorBuild = $matches[2]
-        $lastDigit = [int]$matches[3] + 1
-        $newVersion = "${majorMinorBuild}$lastDigit"
-        $newContent = $content -replace $pattern_ver, ('[assembly: AssemblyVersion("{0}")]' -f $newVersion)
+        if ($content -match $pattern_pro) {
+            $newContent = $newContent -replace $pattern_pro, ('[assembly: AssemblyProduct("{0}")]' -f $ValueFromPipeline_3)
+        }
+        
+        if ($content -match $pattern_ver) {
+            $majorMinorBuild = $matches[2]
+            $lastDigit = [int]$matches[3] + 1
+            $newVersion = "${majorMinorBuild}$lastDigit"
+            $newContent = $newContent -replace $pattern_ver, ('[assembly: AssemblyVersion("{0}")]' -f $newVersion)
+        }
+        
+        if ($content -match $pattern_ver_file) {
+            $majorMinorBuild = $matches[2]
+            $lastDigit = [int]$matches[3] + 1
+            $newVersion = "${majorMinorBuild}$lastDigit"
+            $newContent = $newContent -replace $pattern_ver_file, ('[assembly: AssemblyFileVersion("{0}")]' -f $newVersion)
+        }
+
         $newContent | Set-Content -Path $file.FullName
-    }
-    elseif($file -match $pattern_ver_file){
-        $content = Get-Content -Path $file.FullName
-        $majorMinorBuild = $matches[2]
-        $lastDigit = [int]$matches[3] + 1
-        $newVersion = "${majorMinorBuild}$lastDigit"
-        $newContent = $content -replace $pattern_ver_file, ('[assembly: AssemblyFileVersion("{0}")]' -f $newVersion)
-        $newContent | Set-Content -Path $file.FullName
-    }
-    else {
-        $file
     }
 }
-   
-
-   
-
-# Save the modified content back to AssemblyInfo.cs
-#$newContent | Set-Content -Path $assemblyInfoPath
-
