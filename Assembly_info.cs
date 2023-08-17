5d6997842a67e46e8b88c7f6e224616ec186076b
עילי גם זו לטובה <ilai.gamzu13@gmail.com>
2023-08-17 16:26:08 +0000

    Updated Assembly_info.cs namespace and class

--- Assembly_info.cs
+++ Assembly_info.cs
@@ -2,9 +2,12 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-
+namespace Assembly_info{
+    public class Assembly_info{
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+    }
+}
