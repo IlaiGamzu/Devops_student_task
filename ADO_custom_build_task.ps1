19ec30c76e0b11ad397ac462c92799ce209b4c3f
עילי גם זו לטובה <ilai.gamzu13@gmail.com>
2023-08-20 17:03:28 +0000

    Added 12 files to do part 3

---
+++ ADO_custom_build_task.ps1
@@ -0,0 +1,57 @@
+# Path to the AssemblyInfo.cs file
+#$assemblyInfoPath = "C:\C#_PROGRAM\Devops student task IAI\custom_student_task\copy_project_of_assemblyinfo.cs"
+$assemblyInfoPath = $env:X = Get-Content -Path "$(Build.SourcesDirectory)\sort_list.AssemblyInfo.cs"
+
+# Read the content of AssemblyInfo.cs
+$content = Get-Content -Path $assemblyInfoPath
+$x_version=Get-Content -Path "$(Build.SourcesDirectory)\num_of_version.txt"
+
+# Value to replace with from pipeline
+param(
+    [string]$ValueFromPipeline_1,
+    [string]$ValueFromPipeline_2,
+    [string]$ValueFromPipeline_3
+)
+
+# Define regex patterns to match AssemblyDescription and AssemblyCompany attributes
+$pattern_des = '(\[assembly: System.Reflection.AssemblyTitleAttribute\("")]|(\[assembly: System.Reflection.AssemblyTitleAttribute\(")([^"]*)("\)\])'
+$pattern_com = '(\[assembly: System.Reflection.AssemblyCompanyAttribute\("")]|(\[assembly: [assembly: System.Reflection.AssemblyCompanyAttribute\(")([^"]*)("\)\])'
+$pattern_pro = '(\[assembly: System.Reflection.AssemblyProductAttribute\("")|(\[assembly: System.Reflection.AssemblyProductAttribute\(")([^"]*)("\)\])'
+$pattern_ver = '(\[assembly: System.Reflection.AssemblyVersionAttribute\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
+$pattern_ver_file = '(\[assembly: System.Reflection.AssemblyFileVersionAttribute\(")(\d+\.\d+\.\d+\.)(\d+)("\)\])'
+# Process each line from the file
+$newContent = foreach ($line in $content) {
+    if ($line -match $pattern_des) {
+        $newLine_1 = $line -replace $pattern_des, ('[assembly: System.Reflection.AssemblyTitleAttribute("{0}")]' -f $ValueFromPipeline_1)
+        $newLine_1
+    }
+    elseif ($line -match $pattern_com) {
+        $newLine_2 = $line -replace $pattern_com, ('[assembly: System.Reflection.AssemblyCompanyAttribute("{0}")]' -f $ValueFromPipeline_2)
+        $newLine_2
+    }
+    elseif ($line -match $pattern_pro) {
+        $newLine_2 = $line -replace $pattern_pro, ('[assembly: System.Reflection.AssemblyProductAttribute("{0}")]' -f $ValueFromPipeline_3)
+        $newLine_2
+    }
+    elseif($line -match $pattern_ver){
+        $majorMinorBuild = $matches[2]
+        $lastDigit = [int]$matches[3] + 1
+        $newVersion = "${majorMinorBuild}$lastDigit"
+        $newLine = $line -replace $pattern_ver, ('[assembly: System.Reflection.AssemblyVersionAttribute("{0}")]' -f $newVersion)
+        $newLine
+    }
+    elseif($line -match $pattern_ver_file){
+        $majorMinorBuild = $matches[2]
+        $lastDigit = [int]$matches[3] + 1
+        $newVersion = "${majorMinorBuild}$lastDigit"
+        $newLine = $line -replace $pattern_ver_file, ('[assembly: System.Reflection.AssemblyFileVersionAttribute("{0}")]' -f $newVersion)
+        $newLine
+    }
+    else {
+        $line
+    }
+}
+
+# Save the modified content back to AssemblyInfo.cs
+$newContent | Set-Content -Path $assemblyInfoPath
+
