9b86d39a7ba392932607e7016efd7ae51d99d790
עילי גם זו לטובה <ilai.gamzu13@gmail.com>
2023-08-21 11:39:40 +0000

    Updated ADO_custom_build_task.ps1

--- ADO_custom_build_task.ps1
+++ ADO_custom_build_task.ps1
@@ -1,10 +1,10 @@
-param(
+param( #The value from pipeline
     [string]$ValueFromPipeline_1,
     [string]$ValueFromPipeline_2,
     [string]$ValueFromPipeline_3
 )
 
-# Diagnostic output
+# Check output
 Write-Host "ValueFromPipeline_1 is: $ValueFromPipeline_1"
 Write-Host "ValueFromPipeline_2 is: $ValueFromPipeline_2"
 Write-Host "ValueFromPipeline_3 is: $ValueFromPipeline_3"
@@ -14,74 +14,40 @@ $assemblyInfoPath = Get-ChildItem "$PSScriptRoot\sort_list.AssemblyInfo.cs"
 
 # Read the content of AssemblyInfo.cs
 $content = Get-Content -Path $assemblyInfoPath
-#Write-Host "The content is :" $content
 
-# Simplified regex patterns to match attributes
+
+# Regex for assemblyInfo file
 $pattern_des = '\[assembly: System\.Reflection\.AssemblyTitleAttribute\(".*?"\)\]'
 $pattern_com = '\[assembly: System\.Reflection\.AssemblyCompanyAttribute\(".*?"\)\]'
 $pattern_pro = '\[assembly: System\.Reflection\.AssemblyProductAttribute\(".*?"\)\]'
 $pattern_ver = '\[assembly: System\.Reflection\.AssemblyVersionAttribute\("(\d+\.\d+\.\d+\.)(\d+)"\)\]'
 #$pattern_ver_file  = '\[assembly: System\.Reflection\.AssemblyFileVersionAttribute\("(\d+\.\d+\.\d+\.)(\d+)"\)\]'
-$pattern_test = '\[assembly: System\.Reflection\.AssemblyFileVersionAttribute\("(\d+)\.(\d+)\.(\d+)\.(\d+)"\)\]'
+$pattern_ver_file = '\[assembly: System\.Reflection\.AssemblyFileVersionAttribute\("(\d+)\.(\d+)\.(\d+)\.(\d+)"\)\]'
 
 
-# Process each line from the file
+# The new content that wiil overiding the current version of assembly info 
 $newContent = foreach ($line in $content) {
     Write-Host "Processing line: $line"
     
-        if( $line -match $pattern_des ){
+        if( $line -match $pattern_des ){ #The description of file
             '[assembly: System.Reflection.AssemblyTitleAttribute("{0}")]' -f $ValueFromPipeline_1
         }
-        elseif( $line -match $pattern_com ){
+        elseif( $line -match $pattern_com ){ #The company file
             '[assembly: System.Reflection.AssemblyCompanyAttribute("{0}")]' -f $ValueFromPipeline_2
         }
-         elseif( $line -match $pattern_pro ) {
+         elseif( $line -match $pattern_pro ) { # The product line
             '[assembly: System.Reflection.AssemblyProductAttribute("{0}")]' -f $ValueFromPipeline_3
         }
-        elseif( $line -match $pattern_ver ) {
+        elseif( $line -match $pattern_ver ) { #The version line
             
             $digit_not_changes = $matches[1]
             $lastDigit = [int]$matches[2] + 1
             $newVersion = "${digit_not_changes}$lastDigit"
             ('[assembly: System.Reflection.AssemblyVersionAttribute("{0}")]' -f $newVersion)
         }
-        elseif( $line -match $pattern_test ) {
-
-                #$revision = [int]$matches[4] + 1
-                #$newVersion = "$($matches[1]).$($matches[2]).$($matches[3]).$revision"
-                #$updatedLine = $line -replace "$($matches[0])", ('[assembly: System.Reflection.AssemblyFileVersionAttribute("{0}")]' -f $newVersion)
-                #$updatedLine
-
-            #if ($line-match $pattern_test) {
-                #Write-Host "Full Match: $($matches[0])"
-                #Write-Host "Major Version: $($matches[1])"
-               # Write-Host "Minor Version: $($matches[2])"
-                #Write-Host "Build Number: $($matches[3])"
-                #Write-Host "Revision: $($matches[4])"
-            #} 
-        #else {
-                #Write-Host "No match found."
-            #}
-
-
-
-            # Check if the match happened
-            #Write-Host "Matched the line: $line"
-
-            #$testLine = '[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]'
-            #if($testLine -match $pattern_test) {
-                #Write-Host "Full match: $matches[0]"
-                #Write-Host "First number: $matches[1]"
-                #Write-Host "Second number: $matches[2]"
-                #Write-Host "Third number: $matches[3]"
-                #Write-Host "Fourth number: $matches[4]"
-            #} else {
-                #Write-Host "No match found!"
-            #}
-
-
-            $revision = [int]$matches[4] + 1
-            $newVersion = "$($matches[1]).$($matches[2]).$($matches[3]).$revision"
+        elseif( $line -match $pattern_ver_file ) { #The versionFile
+            $lastdig = [int]$matches[4] + 1
+            $newVersion = "$($matches[1]).$($matches[2]).$($matches[3]).$lastdig"
             ('[assembly: System.Reflection.AssemblyFileVersionAttribute("{0}")]' -f $newVersion)      
         }
         else {
@@ -90,5 +56,5 @@ $newContent = foreach ($line in $content) {
     }
 #}
 
-# Save the modified content back to AssemblyInfo.cs
+# Save the new content back to AssemblyInfo.cs
 $newContent | Set-Content -Path $assemblyInfoPath
